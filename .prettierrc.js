06fb5725206e014e388688461df51744b84af318
ppxb <ppxb123@gmail.com>
2022-10-25 20:05:41 +0800

    first commit

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  semi: false,
+  tabWidth: 2,
+  printWidth: 80,
+  singleQuote: true,
+  quoteProps: 'consistent',
+  vueIndentScriptAndStyle: true,
+  trailingComma: 'none',
+}
---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ArcoResolver } from 'unplugin-vue-components/resolvers'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ArcoResolver()]
+    }),
+    Components({
+      resolvers: [
+        ArcoResolver({
+          sideEffect: true
+        })
+      ]
+    })
+  ],
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: resolve(__dirname, '../src')
+      }
+    ],
+    extensions: ['.ts', '.js']
+  }
+})
